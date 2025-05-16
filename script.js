e9a4e85cf53956d82d638eba5f1d23952bd39f69
aleeksandraaa <aleksandra.valdovska@vidusskolalielvarde.lv>
2025-05-16 11:42:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const passwordField = document.getElementById('password');
+const togglePassword = document.getElementById('toggle-password');
+let visible = false;
+
+togglePassword.addEventListener('click', () => {
+  visible = !visible;
+  passwordField.type = visible ? 'text' : 'password';
+  togglePassword.src = visible ? 'assets/eye-slash-icon.png' : 'assets/eye-icon.png';
+});
